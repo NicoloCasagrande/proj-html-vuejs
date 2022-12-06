d0e236f1baf44e6c1ec8b705a1af5546d69f57d2
Nicolò Casagrande <nicolo.casagrande7@gmail.com>
2022-12-06 14:12:17 +0100

    prima section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,26 @@
 <script>
 import AppHero from "./AppHero.vue";
+import SectionFoodieJournal from "./SectionFoodieJournal.vue";
 export default {
   name: "AppMain",
   components: {
     AppHero,
+    SectionFoodieJournal,
   },
 };
 </script>
 
 <template>
-  <div><AppHero /></div>
+  <div>
+    <AppHero />
+    <div class="container relevant">
+      <SectionFoodieJournal class="" />
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.relevant {
+  margin-top: -6.25rem;
+}
+</style>
---
+++ src/components/SectionFoodieJournal.vue
@@ -0,0 +1,49 @@
+<script>
+import { store } from "../store";
+export default {
+  data() {
+    return {
+      store,
+      items: [
+        {
+          title: "Food Corner: Top Japanese Restaurants for Sushi",
+          date: "March 25th, 2019",
+          image: "../assets/sfondo.jpg",
+          text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, id Facilis asperiores totam repellendus ducimus autem numquam consequuntur quasi dolorum dignissimos. Explicabo laborum accusantium voluptatibus tempore? Assumenda, aliquid cumque? Excepturi!",
+        },
+        {
+          title: "Roundup, My New Favourite Recipes for Healthy Living",
+          date: "March 25th, 2019",
+          image: "../assets/fi-roundup.jpg.jpg",
+          text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, id Facilis asperiores totam repellendus ducimus autem numquam consequuntur quasi dolorum dignissimos. Explicabo laborum accusantium voluptatibus tempore? Assumenda, aliquid cumque? Excepturi!",
+        },
+        {
+          title: "Why These Toast with Tea are My New Favourite",
+          date: "March 25th, 2019",
+          image: "../assets/fi-organic-breakfast.jpg",
+          text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, id Facilis asperiores totam repellendus ducimus autem numquam consequuntur quasi dolorum dignissimos. Explicabo laborum accusantium voluptatibus tempore? Assumenda, aliquid cumque? Excepturi!",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="text-center white-bg">
+    <h3>FOODIE JOURNAL</h3>
+    <div class="row">
+      <div class="col-4" v-for="item in items">
+        <img :src="item.image" :alt="item.title" />
+        <h4>{{ item.title }}</h4>
+        <span>By admin | {{ item.date }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.white-bg {
+  background-color: white;
+}
+</style>
