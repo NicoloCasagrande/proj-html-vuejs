58a7a28ef69d3c5813a478896658a1d37f1ae7d0
Nicolò Casagrande <nicolo.casagrande7@gmail.com>
2022-12-13 11:50:50 +0100

    sistemazione culinarycollection

--- src/components/SectionCulinaryCollection.vue
+++ src/components/SectionCulinaryCollection.vue
@@ -1,42 +1,10 @@
 <script>
+import { store } from "../store";
 export default {
   name: "SectionCulinaryCollection",
   data() {
     return {
-      collection: [
-        {
-          name: "DRINK",
-          image: "src/assets/drinks-recipes.png",
-        },
-        {
-          name: "SOUPS",
-          image: "src/assets/soups-recipes.png",
-        },
-        {
-          name: "BAKERY",
-          image: "src/assets/baking-recipes.png",
-        },
-        {
-          name: "DINNER",
-          image: "src/assets/dinner-recipes.png",
-        },
-        {
-          name: "HEALTHY",
-          image: "src/assets/healthy-recipes.png",
-        },
-        {
-          name: "STAFF PICKS",
-          image: "src/assets/staff-picks.png",
-        },
-        {
-          name: "APPETISERS",
-          image: "src/assets/premium-recipes.png",
-        },
-        {
-          name: "QUICK & EASY",
-          image: "src/assets/quick-easy-recipes.png",
-        },
-      ],
+      store,
     };
   },
 };
@@ -50,10 +18,10 @@ export default {
       animi, laborum error dicta dolores similique tenetur repellendus dolorum
       sunt
     </p>
-    <div class="row">
-      <div class="col-3" v-for="collect in collection">
+    <div class="row mt-4">
+      <div class="col-3" v-for="collect in store.collection">
         <div class="bg-white text-center py-5 mb-4">
-          <img :src="collect.image" :alt="collect.name" />
+          <img :src="collect.image" :alt="collect.name" class="mb-4" />
           <h6>{{ collect.name }}</h6>
         </div>
       </div>
--- src/store.js
+++ src/store.js
@@ -85,4 +85,38 @@ export const store = reactive({
       text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, id Facilis asperiores totam repellendus ducimus autem numquam consequuntur quasi dolorum dignissimos. Explicabo laborum accusantium voluptatibus tempore? Assumenda, aliquid cumque? Excepturi!",
     },
   ],
+  collection: [
+    {
+      name: "DRINK",
+      image: "src/assets/drinks-recipes.png",
+    },
+    {
+      name: "SOUPS",
+      image: "src/assets/soups-recipes.png",
+    },
+    {
+      name: "BAKERY",
+      image: "src/assets/baking-recipes.png",
+    },
+    {
+      name: "DINNER",
+      image: "src/assets/dinner-recipes.png",
+    },
+    {
+      name: "HEALTHY",
+      image: "src/assets/healthy-recipes.png",
+    },
+    {
+      name: "STAFF PICKS",
+      image: "src/assets/staff-picks.png",
+    },
+    {
+      name: "APPETISERS",
+      image: "src/assets/premium-recipes.png",
+    },
+    {
+      name: "QUICK & EASY",
+      image: "src/assets/quick-easy-recipes.png",
+    },
+  ],
 });
